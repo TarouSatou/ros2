1a4c1de7fb230f8418a38d22705967730590f78c
Tarou Satou <zabulangdabianzu@gmail.com>
2022-11-24 12:06:42 +0900

    Initial Commit

---
+++ mypkg/talker.py
@@ -0,0 +1,15 @@
+import rclpy
+from rclpy.node import Node
+from person_msgs.srv import Query #使う型を変更
+
+def cb(request, response):
+    if request.name == "佐藤太郎":
+        response.age = 44
+    else:
+        response.age = 255
+
+    return response
+rclpy.init()
+node = Node("talker")
+srv = node.create_service(Query, "query", cb) #サービスの作成
+rclpy.spin(node)
